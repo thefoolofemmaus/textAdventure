c90b8ef089586a8c68cea18d97b46ac83c8aa505
Michael Corey <thefoolofemmaus@gmail.com>
2025-02-14 16:29:03 -0600

    Adding springboot boilerplate

---
+++ textProcessor/textProcessor/src/main/java/com/textadventure/textProcessor/TextProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.textadventure.textProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorApplication.class, args);
+	}
+
+}
---
+++ textProcessor/textProcessor/src/test/java/com/textadventure/textProcessor/TextProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textadventure.textProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
